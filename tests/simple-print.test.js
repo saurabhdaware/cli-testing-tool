c4237e03bf8b53c45411e11f7421028a93cdf5ce
saurabhdaware <saurabhdaware99@gmail.com>
2021-09-29 22:13:31 +0530

    add ansi char map

--- tests/simple-print.test.js
+++ tests/simple-print.test.js
@@ -2,10 +2,11 @@ const path = require('path');
 const { createCommandInterface } = require('../lib');
 
 test('should pass', async () => {
-  const commandInterface = createCommandInterface('node ./experiment.js', {
+  const commandInterface = createCommandInterface('node ./simple-print.js', {
     cwd: path.join(__dirname, '..', 'cli-examples'),
   });
-  await commandInterface.type('Saurabh\n22\n');
+  await commandInterface.type('Saurabh\n');
+  await commandInterface.type('22\n');
   const terminal = await commandInterface.getOutput();
 
   expect(terminal.textAfter("What's your name?")).toBe('Hi, Saurabh!'); // Hi, Saurabh!
