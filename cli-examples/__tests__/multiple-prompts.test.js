b26ed0a5e7ffdf5085472826f76900a60b678708
saurabhdaware <saurabhdaware99@gmail.com>
2021-10-14 00:00:32 +0530

    add typeDelay option

---
+++ cli-examples/__tests__/multiple-prompts.test.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const { createCommandInterface } = require('../../lib');
+
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+test('should pass', async () => {
+  const commandInterface = createCommandInterface('node ./multiple-prompts.js', {
+    cwd: path.join(__dirname, '..'),
+    logData: true,
+  });
+  await commandInterface.type('19\n');
+  // await wait(100);
+  await commandInterface.type('saurabh\n');
+  const terminal = await commandInterface.getOutput();
+  console.log(terminal);
+  expect(1).toBe(1);
+});
