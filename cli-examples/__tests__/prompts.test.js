4acf60acad17fa357a8f20a8e7f5719cee3cfd8b
saurabhdaware <saurabhdaware99@gmail.com>
2021-10-16 13:55:54 +0530

    fix tests for windows

--- cli-examples/__tests__/prompts.test.js
+++ cli-examples/__tests__/prompts.test.js
@@ -1,11 +1,16 @@
-const path = require('path');
-const { createCommandInterface } = require('../../lib');
+const path = require("path");
+const { createCommandInterface } = require("../../lib");
 
-test('should block 12 year old', async () => {
-  const commandInterface = createCommandInterface('node ./prompts.js', {
-    cwd: path.join(__dirname, '..'),
+// Windows is just weird.
+const FORWARD_ARROW = process.platform === "win32" ? "»" : "›";
+
+test("should block 12 year old", async () => {
+  const commandInterface = createCommandInterface("node ./prompts.js", {
+    cwd: path.join(__dirname, ".."),
   });
-  await commandInterface.type('12\n');
+  await commandInterface.type("12\n");
   const terminal = await commandInterface.getOutput();
-  expect(terminal.stringOutput).toBe('? How old are you? › 12\n› Nightclub is 18+ only');
+  expect(terminal.stringOutput).toBe(
+    `? How old are you? ${FORWARD_ARROW} 12\n${FORWARD_ARROW} Nightclub is 18+ only`
+  );
 });
--- cli-examples/multiple-prompts.js
+++ cli-examples/multiple-prompts.js
@@ -1,19 +1,19 @@
-const prompts = require('prompts');
+const prompts = require("prompts");
 
 (async () => {
   const response1 = await prompts({
-    type: 'number',
-    name: 'value',
-    message: 'How old are you?',
-    validate: value => value < 18 ? `Nightclub is 18+ only` : true
+    type: "number",
+    name: "value",
+    message: "How old are you?",
+    validate: (value) => (value < 18 ? `Nightclub is 18+ only` : true),
   });
 
   console.log(response1); // => { value: 24 }
 
   const response2 = await prompts({
-    type: 'text',
-    name: 'value',
-    message: 'What is your name?',
+    type: "text",
+    name: "value",
+    message: "What is your name?",
   });
 
   console.log(response2);
